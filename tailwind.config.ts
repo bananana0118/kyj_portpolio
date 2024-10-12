693a1d125d1606043345a78f4c20f3edf196d2e1
bananana0118 <bananana0118@gmail.com>
2024-10-12 19:22:58 +0900

    feat : 스타일링 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,28 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            screens: {
+                xs: "480px",
+            },
+            colors: {
+                primary: "#222831",
+                secondary: "#393E46",
+                highlight: "#00ADB5",
+                fontWhite: "#EEEEEE",
+                fontBlack: "#222831",
+
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
